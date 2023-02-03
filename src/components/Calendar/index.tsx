38cbd5168e3cdd9f83bf2e963e4adeb061335d06
Anderson <anderson@pop-os.localdomain>
2023-02-03 13:42:24 -0300

    Estrutura visual da lista de horarios

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -61,6 +61,29 @@ export function Calendar() {
               <CalendarDay>3</CalendarDay>
             </td>
           </tr>
+          <tr>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
         </tbody>
       </CalendarBody>
     </CalendarContainer>
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,38 @@
 import { Calendar } from '@/components/Calendar'
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            Terça-feira <span>20 de Setembro</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
