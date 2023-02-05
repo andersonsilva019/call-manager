87979cf2299a8553c244fd23a2a76310800557aa
Anderson <anderson@pop-os.localdomain>
2023-02-05 08:19:17 -0300

    Preparando calendario

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -21,7 +21,12 @@ interface CalendarWeek {
 
 type CalendarWeeks = CalendarWeek[]
 
-export function Calendar() {
+interface CalendarProps {
+  selectedDate?: Date | null
+  onDateSelect: (date: Date) => void
+}
+
+export function Calendar({ selectedDate, onDateSelect }: CalendarProps) {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set('date', 1)
   })
@@ -73,7 +78,7 @@ export function Calendar() {
         return { date, disabled: true }
       }),
       ...daysInMonthArray.map((date) => {
-        return { date, disabled: false }
+        return { date, disabled: date.endOf('day').isBefore(new Date()) }
       }),
       ...nextMonthFillArray.map((date) => {
         return { date, disabled: true }
@@ -98,8 +103,6 @@ export function Calendar() {
 
     return calendarWeeks
   }, [currentDate])
-
-  console.log(calendarWeeks)
 
   return (
     <CalendarContainer>
@@ -133,7 +136,10 @@ export function Calendar() {
                 {days.map(({ date, disabled }) => {
                   return (
                     <td key={date.toString()}>
-                      <CalendarDay disabled={disabled}>
+                      <CalendarDay
+                        disabled={disabled}
+                        onClick={() => onDateSelect(date.toDate())}
+                      >
                         {date.get('date')}
                       </CalendarDay>
                     </td>
